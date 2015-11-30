9cb75f4e6252e833df7f60f441b0be22c2be6035
Tyler Camp <tyler.camp@loop.colum.edu>
2015-11-30 15:25:44 -0600

    Basic attempting at parallelizing chunking process (after chunk tagging), not yet working (std::vector not thread-safe)

--- QuickData/FbxPartitioning/Workflows/MeshChunkingWorkflow.h
+++ QuickData/FbxPartitioning/Workflows/MeshChunkingWorkflow.h
@@ -3,8 +3,61 @@
 #include <iostream>
 
 #include "../Types.h"
+#include "../../QuickData.Unity/ThreadPool.h"
 
 
+void chunk_mesh_mt( gpu_triangle_array mesh, cpu_chunk_array * bounded_chunks )
+{
+	auto & chunks = *bounded_chunks;
+	std::atomic_size_t numProcessed = 0;
+	std::atomic_size_t numJoined = 0;
+	
+	//	BROKEN: Vector not thread-safe
+
+	auto kernel = [mesh, &numProcessed, &numJoined]( mesh_chunk & chunk )
+	{
+		auto & bounds = chunk.bounds;
+
+		auto meshData = mesh.data( );
+
+		for( int i = 0; i < mesh.extent.size( ); i++ )
+		{
+			if( bounds.contains_point( meshData[i].center ) )
+				chunk.tris.push_back( meshData[i] );
+			++numProcessed;
+		}
+
+		++numJoined;
+	};
+
+	ThreadPool pool( 4 );
+
+	std::atomic_size_t totalJobs = chunks.size( ) * static_cast<unsigned long long>(mesh.extent.size( ));
+
+	for( int i = 0; i < chunks.size( ); i++ )
+		pool.enqueue( kernel, chunks[i] );
+
+	//	Join all, show progress
+	float lastCompletion = 0.0f;
+	auto progressPoints = { 0.1f, 0.2f, 0.3f, 0.4f, 0.5f, 0.6f, 0.7f, 0.8f, 0.9f, 1.0f, 1.1f, 1.2f, 1.3f };
+	while( numJoined < chunks.size( ) )
+	{
+		int currentCompleted = numProcessed;
+		float currentCompletion = currentCompleted / (double)totalJobs;
+
+		Sleep( 10 );
+
+		for( auto point : progressPoints )
+		{
+			if( lastCompletion < point && currentCompletion > point )
+				std::cout << (int)round( point * 100.0f ) << "%.. ";
+		}
+
+		lastCompletion = currentCompletion;
+	}
+
+	std::cout << "Done." << std::endl;
+}
 
 void chunk_mesh( gpu_triangle_array mesh, cpu_chunk_array * out_chunks )
 {
@@ -18,14 +71,14 @@ void chunk_mesh( gpu_triangle_array mesh, cpu_chunk_array * out_chunks )
 
 		for( int p = 0; p < chunks.size( ); p++ )
 		{
-			if( chunks[p].num_tris == chunks[p].tris.size( ) )
+			if( chunks.data()[p].num_tris == chunks.data()[p].tris.size( ) )
 				continue;
 
-			auto & desc = chunks[p].bounds;
+			auto & desc = chunks.data()[p].bounds;
 
-			if( desc.contains_point( mesh[i].center ) )
+			if( desc.contains_point( mesh.data()[i].center ) )
 			{
-				chunks[p].tris.push_back( mesh[i] );
+				chunks[p].tris.push_back( mesh.data()[i] );
 				break;
 			}
 		}
@@ -34,7 +87,7 @@ void chunk_mesh( gpu_triangle_array mesh, cpu_chunk_array * out_chunks )
 
 void workflow_chunk_from_partitions( gpu_triangle_array * tris, gpu_partition_descriptor_array * partitions, gpu_index_array * tags, gpu_index_array * partitionSizes, cpu_chunk_array ** out_chunks )
 {
-	//	TODO - Could parallelize (sort data by partition?)
+	//	TODO - Could GPU parallelize (sort data by partition?)
 	auto & dev_mesh = *tris;
 	auto & dev_partitions = *partitions;
 	auto & dev_partitionSizes = *partitionSizes;
@@ -50,6 +103,7 @@ void workflow_chunk_from_partitions( gpu_triangle_array * tris, gpu_partition_de
 	}
 
 	chunk_mesh( dev_mesh, chunks );
+	//chunk_mesh_mt( dev_mesh, chunks );
 
 	*out_chunks = chunks;
 }
